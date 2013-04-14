301af53fb83d30bb90d7ccce9f941beeb79d1861
P Guardiario <pguardiario@gmail.com>
2013-04-14 15:54:30 +0800

    initial commit

--- pgtable.php
+++ pgtable.php
@@ -5,6 +5,23 @@ abstract class PGTable{
   protected static $conn = null;
   protected static $table_name = null;
 
+  function __construct($agent_id = null){
+    $this->attributes = array();
+  }
+
+  // private functions
+  private function sprintf_key_value($key, $value){
+    switch(gettype($value)){
+      case 'integer':
+      case 'double':
+        return sprintf("`%s`=%d", mysql_real_escape_string($key), mysql_real_escape_string($value));
+      case 'string': return sprintf("`%s`='%s'", mysql_real_escape_string($key), mysql_real_escape_string($value));
+      case 'NULL': return sprintf("`%s`=null", mysql_real_escape_string($key));
+      default: die("unknown type: $value - " . gettype($value));
+    }
+  }
+
+  // static functions
   public static function initialize($user, $password, $database, $host = 'localhost'){
     // check version
     if(version_compare(PHP_VERSION, '5.3.0') < 0) die('Error: PGTable requires php version 5.3.0 or greater.');
@@ -13,38 +30,7 @@ abstract class PGTable{
     mysql_select_db($database, self::$conn) or die('Could not select $db: ' . mysql_error());
   }
 
-  function __construct($agent_id = null){
-    $this->attributes = array();
-  }
-
-  function update_attributes($attributes){
-    foreach($attributes as $key => $value){
-      $this->attributes[$key] = $value;
-    }
-  }
-
-  function save(){
-    if(!isset($this->attributes['id'])){
-      mysql_query("insert into " . static::$table_name . " (id) values (0)") or die('Invalid query: ' . mysql_error());
-      $this->attributes['id'] = mysql_insert_id();
-    }
-    $vals = array();
-    foreach($this->attributes as $key => $value){
-      if($key == 'id') continue;
-      switch(gettype($value)){
-        case 'integer':
-        case 'double':
-          $vals[] = sprintf("`%s`=%d", mysql_real_escape_string($key), mysql_real_escape_string($value)); break;
-        case 'string': $vals[] = sprintf("`%s`='%s'", mysql_real_escape_string($key), mysql_real_escape_string($value)); break;
-        case 'NULL': $vals[] = sprintf("`%s`=null", mysql_real_escape_string($key)); break;
-        default: die("unknown type: $value - " . gettype($value));
-      }
-    }
-    $sql = "update " . static::$table_name . " set " . implode(', ', $vals). " where id=".$this->attributes['id'];
-    mysql_query($sql) or die('Invalid query: ' . mysql_error());
-  }
-
-  function find_by_sql($sql){
+  public static function find_by_sql($sql){
     $result = mysql_query($sql, self::$conn) or die('Invalid query: ' . mysql_error());
     if($row = mysql_fetch_array($result, MYSQL_ASSOC)){
       $class = get_called_class();
@@ -56,7 +42,7 @@ abstract class PGTable{
     }
   }
 
-  function find_all_by_sql($sql){
+  public static function find_all_by_sql($sql){
     $result = mysql_query($sql, self::$conn) or die('Invalid query: ' . mysql_error());
     $ret = array();
     $class = get_called_class();
@@ -68,17 +54,17 @@ abstract class PGTable{
     return $ret;
   }
 
-  function find($id){
+  public static function find($id){
     $sql = "select * from " . static::$table_name . " where id = $id";
     return self::find_by_sql($sql);
   }
 
-  function all(){
+  public static function all(){
     $sql = "select * from " . static::$table_name;
     return self::find_all_by_sql($sql);
   }
 
-  function found_rows(){
+  public static function found_rows(){
     $row = mysql_fetch_array(mysql_query('select found_rows()', self::$conn));
     return $row[0];
   }
@@ -92,14 +78,7 @@ abstract class PGTable{
       $vals = array();
       foreach($keys as $key){
         $value = array_shift($values);
-        switch(gettype($value)){
-          case 'integer':
-          case 'double':
-            $vals[] = sprintf("`%s`=%d", mysql_real_escape_string($key), mysql_real_escape_string($value)); break;
-          case 'string': $vals[] = sprintf("`%s`='%s'", mysql_real_escape_string($key), mysql_real_escape_string($value)); break;
-          case 'NULL': $vals[] = sprintf("`%s`=null", mysql_real_escape_string($key)); break;
-          default: die("unknown type: $value - " . gettype($value));
-        }
+        $vals[] = self::sprintf_key_value($key, $value);
       }
 
       $sql = "select * from " . static::$table_name . " where " . implode(' and ', $vals);
@@ -110,6 +89,28 @@ abstract class PGTable{
       }
     }
   }
+
+  // instance functions
+  public function update_attributes($attributes){
+    foreach($attributes as $key => $value){
+      $this->attributes[$key] = $value;
+    }
+  }
+
+  public function save(){
+    if(!isset($this->attributes['id'])){
+      mysql_query("insert into " . static::$table_name . " (id) values (0)") or die('Invalid query: ' . mysql_error());
+      $this->attributes['id'] = mysql_insert_id();
+    }
+    $vals = array();
+    foreach($this->attributes as $key => $value){
+      if($key == 'id') continue;
+      $vals[] = $this->sprintf_key_value($key, $value);
+    }
+    $sql = "update " . static::$table_name . " set " . implode(', ', $vals). " where id=".$this->attributes['id'];
+    mysql_query($sql) or die('Invalid query: ' . mysql_error());
+  }
+
 }
 
 ?>
